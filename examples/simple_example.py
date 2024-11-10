27454c68b5ca838d972f8adbbeb6b4b364d4c353
opencommerce-xyz <idan@opencommerce.xyz>
2024-11-10 19:59:27 +0200

    added .py to example

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -10,6 +10,5 @@ address = sdk.get_account_address()
 print(f"My account address: {address}")
 
 # Use a service
-# you can use any service from the service directory (https://github.com/opencommerce-xyz/sdk/service-directory)
 service_response = sdk.use_service('gpt_researcher', {'query': 'tell me about Doland Trump'})
 print(f"Service response: {service_response}")
